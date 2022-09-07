ebc251cb9f3176785fde48fa0f8401fd1e50b8c1
Dannybobo <hhyoyo789@gmail.com>
2022-09-08 01:56:59 +0800

    Upload codes

---
+++ NewsApp20220524/app/src/main/java/com/practice/newsapp/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.practice.newsapp;
+
+import com.practice.newsapp.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ NewsApp20220524/app/src/main/java/com/practice/newsapp/SelectListener.java
@@ -0,0 +1,7 @@
+package com.practice.newsapp;
+
+import com.practice.newsapp.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
